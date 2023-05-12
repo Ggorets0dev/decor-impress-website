d29e064e766744424790bf8ca8251b7646d4e345
Ggorets0 <nikgorets4work@gmail.com>
2023-05-12 20:42:14 +0300

    A static folder with the future frontend is connected

--- controllers/download.js
+++ controllers/download.js
@@ -1,6 +1,6 @@
 import path from 'path'
 
 export const sendPriceList = (req, res) => {
-    const priceListPath = path.resolve(path.resolve(), 'download', 'Актуальные цены Valpaint.pdf')
+    const priceListPath = path.resolve(path.resolve(), 'static', 'download', 'Актуальные цены Valpaint.pdf')
     res.download(priceListPath)
 }
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,3 @@
-import path from 'path'
-
 export const showMainPage = (req, res) => {
-    const mainPagePath = path.resolve(path.resolve(), 'static', 'index.html')
-    res.sendFile(mainPagePath)
+    res.sendFile('index.html')
 }
