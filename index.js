ab850a6e51beb1ac92f8307042bda9929e7cb629
Ggorets0dev <nikgorets4work@gmail.com>
2023-05-12 12:48:49 +0300

    Added directory upload button handler, created a directory for storing files sent to the client

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ import * as dotenv from 'dotenv'
 import path from 'path'
 
 dotenv.config()
+
+const __dirname = path.resolve()
 const PORT = process.env.PORT ?? 3000
-const MAIN_PAGE_PATH = path.resolve(path.resolve(), 'static', 'index.html')
+const MAIN_PAGE_PATH = path.resolve(__dirname, 'static', 'index.html')
+const PRICE_LIST_PATH = path.resolve(__dirname, 'download', 'Актуальные цены Valpaint.pdf')
 
 const app = express()
 
 app.get('/', (req, res) => {
     res.sendFile(MAIN_PAGE_PATH)
+})
+
+app.get('/download', (req, res) => {
+    res.download(PRICE_LIST_PATH)
 })
 
 app.listen(PORT, () => {
